b5814c2b79e858ac5a26f57e7ca15f3b854e2d2c
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-27 12:31:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let countDisplay = document.getElementById('count')
+let incrementBtn = document.getElementById('incrementBtn')
+
+var count = 0
+function handleClick (){
+	count++
+	countDisplay.textContent = count
+}
+document.addEventListener('click', handleClick)
